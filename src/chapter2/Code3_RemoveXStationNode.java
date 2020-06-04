9284c0bcfd9d77211288083307aef8e9053672d4
guofucheng <guofucheng@ruc.edu.cn>
2020-06-04 22:29:36 +0800

    chapter2链表部分题目，完成3、删除中间位置节点，删除a/b位置节点

--- src/chapter2/Code3_RemoveXStationNode.java
+++ src/chapter2/Code3_RemoveXStationNode.java
@@ -7,4 +7,52 @@ package chapter2;
  */
 public class Code3_RemoveXStationNode {
 
+    public class Node{
+        public int value;
+        public Node next;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public static Node removeMidNode(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (head.next.next == null) {
+            return head.next;
+        }
+        Node pre = head;
+        Node cur = head.next.next;
+
+        while (cur.next != null && cur.next.next != null) {
+            cur = cur.next.next;
+            pre = pre.next;
+        }
+        pre.next = pre.next.next;
+        return head;
+    }
+
+    public static Node removeNodeByRatio(Node head, int a, int b){
+        if (a > b || a < 1) {
+            return head;
+        }
+        int n = 0;
+        Node cur = head;
+        while (cur != null){
+            n++;
+            cur = cur.next;
+        }
+        n = (int) Math.ceil( (double)(a * n) / (double)b );
+        if (n == 1){
+            head = head.next;
+        }else {
+            cur = head;
+            while (--n != 1){
+                cur = cur.next;
+            }
+            cur.next = cur.next.next;
+        }
+        return head;
+    }
 }
